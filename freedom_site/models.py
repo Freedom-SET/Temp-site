7c25512d068d80bf3d6eefccf3055ae6f3640285
= <brahakishore@gmail.com>
2019-03-25 02:59:09 +0530

    Allow semester to be null

--- freedom_site/models.py
+++ freedom_site/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Account(models.Model):
-    semester = models.CharField(max_length=3)
     name = models.CharField(max_length=20)
+    semester = models.CharField(max_length=3, null=True)
     email = models.EmailField(primary_key=True)
     phone = models.CharField(max_length=10, unique=True)
     usn = models.CharField(max_length=11, unique=True)
